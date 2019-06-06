fd83a5f9c7d95f666726a5902b42224385e65cda
Kirill Titov <kirill@kirilltitov.com>
2019-06-06 10:14:23 +0000

    Milestone

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ DOCKER_COMPOSE_FILE="docker/docker-compose.yaml"
 PRIVATE_IP=`ifconfig eth1 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}'`
 
 export LGNS_HOST=$PRIVATE_IP
-export LGNS_PORT=1711
+export LGNS_PORT=1712
 export HTTP_HOST=127.0.0.1
-export HTTP_PORT=8003
+export HTTP_PORT=8081
 
 docker pull kirilltitov/elegion:quorum_latest
 
